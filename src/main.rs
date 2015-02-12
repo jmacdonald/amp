2fcd12427924890315a734e1a728638a8f0d6102
Jordan MacDonald <jordan@wastedintelligence.com>
2015-02-12 00:17:10 -0500

    Implement *very* basic normal/input modes, as well as save command.

--- src/main.rs
+++ src/main.rs
@@ -11,27 +11,49 @@ fn main() {
     let view = view::new();
     view.display(buffer.data().as_slice());
     view.set_cursor(&*buffer.cursor);
+    let mut insert = false;
 
     loop {
         match view.get_input() {
-            Some('q') => break,
-            Some('j') => {
-                buffer.cursor.move_down();
-                view.set_cursor(&*buffer.cursor);
+            Some(c) => {
+                if insert {
+                    if c == '\\' {
+                        insert = false
+                    } else {
+                        buffer.insert(c.to_string().as_slice());
+                        buffer.cursor.move_right();
+                        view.set_cursor(&*buffer.cursor);
+                        view.display(buffer.data().as_slice());
+                    }
+                } else {
+                    match c {
+                        'q' => break,
+                        'j' => {
+                            buffer.cursor.move_down();
+                            view.set_cursor(&*buffer.cursor);
+                        },
+                        'k' => {
+                            buffer.cursor.move_up();
+                            view.set_cursor(&*buffer.cursor);
+                        },
+                        'h' => {
+                            buffer.cursor.move_left();
+                            view.set_cursor(&*buffer.cursor);
+                        },
+                        'l' => {
+                            buffer.cursor.move_right();
+                            view.set_cursor(&*buffer.cursor);
+                        },
+                        'i' => {
+                            insert = true;
+                        },
+                        's' => {
+                            buffer.save();
+                        },
+                        _ => continue,
+                    }
+                }
             },
-            Some('k') => {
-                buffer.cursor.move_up();
-                view.set_cursor(&*buffer.cursor);
-            },
-            Some('h') => {
-                buffer.cursor.move_left();
-                view.set_cursor(&*buffer.cursor);
-            },
-            Some('l') => {
-                buffer.cursor.move_right();
-                view.set_cursor(&*buffer.cursor);
-            },
-            Some(_) => {},
             None => {},
         }
     }
