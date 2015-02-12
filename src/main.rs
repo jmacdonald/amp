ac53886226cd40672dfbc938239201704c90bc9d
Jordan MacDonald <jordan@wastedintelligence.com>
2015-02-11 22:40:51 -0500

    Use scribe's cursor movement methods.

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,25 @@ fn main() {
     let mut buffer = scribe::buffer::from_file(&Path::new("./Cargo.toml")).unwrap();
     let view = view::new();
     view.display(buffer.data().as_slice());
+    view.set_cursor(&*buffer.cursor);
 
     loop {
         match view.get_input() {
             Some('q') => break,
             Some('j') => {
-                let position = scribe::buffer::Position{ line: buffer.cursor.line+1, offset: buffer.cursor.offset };
-                buffer.move_cursor(position);
+                buffer.cursor.move_down();
                 view.set_cursor(&*buffer.cursor);
             },
             Some('k') => {
-                let position = scribe::buffer::Position{ line: buffer.cursor.line-1, offset: buffer.cursor.offset };
-                buffer.move_cursor(position);
+                buffer.cursor.move_up();
                 view.set_cursor(&*buffer.cursor);
             },
             Some('h') => {
-                let position = scribe::buffer::Position{ line: buffer.cursor.line, offset: buffer.cursor.offset-1 };
-                buffer.move_cursor(position);
+                buffer.cursor.move_left();
                 view.set_cursor(&*buffer.cursor);
             },
             Some('l') => {
-                let position = scribe::buffer::Position{ line: buffer.cursor.line, offset: buffer.cursor.offset+1 };
-                buffer.move_cursor(position);
+                buffer.cursor.move_right();
                 view.set_cursor(&*buffer.cursor);
             },
             Some(_) => {},
