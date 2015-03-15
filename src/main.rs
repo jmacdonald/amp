c033653e7f8348de33c053a87018602badd2c213
Jordan MacDonald <jordan@wastedintelligence.com>
2015-03-15 15:42:01 -0400

    Updated to use a workspace (albeit limitedly).

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,20 @@ use std::os;
 mod view;
 
 fn main() {
+    // Set up a workspace in the current directory.
+    let mut workspace = match os::getcwd() {
+        Ok(path) => scribe::workspace::new(path),
+        Err(error) => panic!("Could not initialize workspace to the current directory."),
+    };
+
+    // Try to open the specified file.
+    // TODO: Handle non-existent files as new empty buffers.
     let path = os::args()[1].clone();
-    let mut buffer = scribe::buffer::from_file(&Path::new(path)).unwrap();
+    let mut argument_buffer = scribe::buffer::from_file(Path::new(path)).unwrap();
+    workspace.add_buffer(argument_buffer);
+
     let view = view::new();
+    let mut buffer = workspace.current_buffer().unwrap();
     view.display(buffer.tokens());
     view.set_cursor(&*buffer.cursor);
     let mut insert = false;
