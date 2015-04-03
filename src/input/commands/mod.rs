730f3bd5228a6373ef0235ac40c8dce30f143ceb
Jordan MacDonald <jordan@wastedintelligence.com>
2015-04-02 21:51:47 -0400

    Implement Command type for function and character pairing.

--- src/input/commands/mod.rs
+++ src/input/commands/mod.rs
@@ -1,4 +1,17 @@
+use application::Application;
+
 pub mod application;
 pub mod workspace;
 pub mod buffer;
 pub mod cursor;
+
+pub struct Command {
+    pub data: char,
+    pub operation: fn(&mut Application),
+}
+
+impl Command {
+    pub fn execute(&self, app: &mut Application) {
+        (self.operation)(app);
+    }
+}
