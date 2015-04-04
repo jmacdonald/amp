859f0a231de7e80039001495f5c5a12a05451165
Jordan MacDonald <jordan@wastedintelligence.com>
2015-04-04 01:16:42 -0400

    Populate application mode with a JumpMode instance when switching to it.

--- src/input/commands/application.rs
+++ src/input/commands/application.rs
@@ -1,5 +1,6 @@
 use application::Mode;
 use application::Application;
+use application::jump_mode;
 
 pub fn switch_to_normal_mode(app: &mut Application, _: &char) {
     app.mode = Mode::Normal;
@@ -9,7 +10,7 @@ pub fn switch_to_insert_mode(app: &mut Application, _: &char) {
 }
 
 pub fn switch_to_jump_mode(app: &mut Application, _: &char) {
-    app.mode = Mode::Jump;
+    app.mode = Mode::Jump(jump_mode::new());
 }
 
 pub fn exit(app: &mut Application, _: &char) {
