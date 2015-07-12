32c5273b69e02bea045fde7de6c3c5dea9b29d51
Jordan MacDonald <jordan@wastedintelligence.com>
2015-07-12 01:39:00 -0400

    Implement open mode "open" command.

--- src/input/commands/open_mode.rs
+++ src/input/commands/open_mode.rs
@@ -1,14 +1,32 @@
 extern crate bloodhound;
+extern crate scribe;
 
 use application::Mode;
 use application::Application;
+use input::commands;
 
 pub fn open(app: &mut Application) {
     match app.mode {
-        Mode::Open(ref open_mode) => {
+        Mode::Open(ref mut mode) => {
+            match mode.results {
+                Some(ref results) => {
+                    let ref selection = results[mode.selected_result_index];
+                    match scribe::buffer::from_file(selection.path.clone()) {
+                        Ok(buffer) => {
+                            app.workspace.add_buffer(buffer);
+                        },
+                        Err(_) => (),
+                    }
+                },
+                None => (),
+            }
         },
         _ => (),
     }
+
+    // FIXME: This should be moved into Ok match result, but we cannot
+    // lend out a reference to app once we've matched against its mode.
+    commands::application::switch_to_normal_mode(app);
 }
 
 pub fn search(app: &mut Application) {
