b896632b5db0f71f0332ac2baf0f0a1065bf937b
Jordan MacDonald <jordan@wastedintelligence.com>
2015-07-21 00:31:50 -0400

    Add up/down key support for open mode entry navigation. Format matches.

--- src/input/modes/open.rs
+++ src/input/modes/open.rs
@@ -4,10 +4,6 @@ use rustbox::keyboard::Key;
 
 pub fn handle(mode: &mut OpenMode, input: Key) -> Option<Command> {
     match input {
-        Key::Esc       => Some(application::switch_to_normal_mode),
-        Key::Enter     => Some(open_mode::open),
-        Key::Ctrl('j') => Some(open_mode::select_next_path),
-        Key::Ctrl('k') => Some(open_mode::select_previous_path),
         Key::Backspace => {
             // Delete the last character of the search term.
             mode.input.pop();
@@ -15,13 +11,17 @@ pub fn handle(mode: &mut OpenMode, input: Key) -> Option<Command> {
             // Re-run the search.
             Some(open_mode::search)
         },
-        Key::Char(c)   => {
+        Key::Char(c) => {
             // Add a character to the search term.
             mode.input.push(c);
 
             // Re-run the search.
             Some(open_mode::search)
         },
-        _              => None,
+        Key::Down | Key::Ctrl('j') => Some(open_mode::select_next_path),
+        Key::Up   | Key::Ctrl('k') => Some(open_mode::select_previous_path),
+        Key::Enter                 => Some(open_mode::open),
+        Key::Esc                   => Some(application::switch_to_normal_mode),
+        _                          => None,
     }
 }
