adac46d05a4a7c4f1f4bb50e0a62ccef560d92ac
Jordan MacDonald <jordan@wastedintelligence.com>
2015-07-21 00:06:28 -0400

    Run a search when entering open mode. Refactor command references.

--- src/input/commands/application.rs
+++ src/input/commands/application.rs
@@ -1,14 +1,14 @@
 use application::Mode;
 use application::Application;
 use application::modes::{insert, jump, open};
-use super::buffer;
+use input::commands;
 
 pub fn switch_to_normal_mode(app: &mut Application) {
-    buffer::end_command_group(app);
+    commands::buffer::end_command_group(app);
     app.mode = Mode::Normal;
 }
 pub fn switch_to_insert_mode(app: &mut Application) {
-    buffer::start_command_group(app);
+    commands::buffer::start_command_group(app);
     app.mode = Mode::Insert(insert::new());
 }
 
@@ -18,6 +18,7 @@ pub fn switch_to_jump_mode(app: &mut Application) {
 
 pub fn switch_to_open_mode(app: &mut Application) {
     app.mode = Mode::Open(open::new(app.workspace.path.clone()));
+    commands::open_mode::search(app);
 }
 
 pub fn exit(app: &mut Application) {
