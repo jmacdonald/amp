b09300260be9e2e99f59d1ea9982003184f9a19a
Jordan MacDonald <jordan@wastedintelligence.com>
2015-09-23 22:58:47 -0400

    Add copy support!

--- src/commands/selection.rs
+++ src/commands/selection.rs
@@ -30,3 +30,30 @@ pub fn change(app: &mut Application) {
     delete(app);
     application::switch_to_insert_mode(app);
 }
+
+pub fn copy(app: &mut Application) {
+    match app.workspace.current_buffer() {
+        Some(buffer) => {
+            match app.mode {
+                Mode::Select(ref select_mode) => {
+                    let cursor_position = *buffer.cursor.clone();
+                    let selected_range = range::new(
+                        cursor_position,
+                        select_mode.anchor
+                    );
+
+                    match buffer.read(&selected_range.clone()) {
+                        Some(selected_data) => {
+                            app.clipboard.set_contents(selected_data);
+                        },
+                        None => ()
+                    }
+                },
+                _ => ()
+            };
+        },
+        None => (),
+    };
+
+    application::switch_to_normal_mode(app);
+}
