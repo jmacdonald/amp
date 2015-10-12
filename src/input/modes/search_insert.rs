4357308bc39c1408a74c0b98b6137e448ef485f7
Jordan MacDonald <jordan@wastedintelligence.com>
2015-10-12 15:29:36 -0400

    Add backspace functionality to search insert mode.

--- src/input/modes/search_insert.rs
+++ src/input/modes/search_insert.rs
@@ -4,9 +4,15 @@ use rustbox::keyboard::Key;
 
 pub fn handle(mode: &mut SearchInsertMode, input: Key) -> Option<Command> {
     match input {
-        Key::Esc     => Some(application::switch_to_normal_mode),
-        Key::Enter   => Some(application::switch_to_search_results_mode),
-        Key::Char(c) => {
+        Key::Esc       => Some(application::switch_to_normal_mode),
+        Key::Enter     => Some(application::switch_to_search_results_mode),
+        Key::Backspace => {
+            // Remove a character from the search term.
+            mode.input.pop();
+
+            None
+        }
+        Key::Char(c)   => {
             // Add a character to the search term.
             mode.input.push(c);
 
