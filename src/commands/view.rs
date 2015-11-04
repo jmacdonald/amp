65bbb56f4ea208ba87ead828bb504f0b39830710
Jordan MacDonald <jordan@wastedintelligence.com>
2015-11-03 22:56:07 -0500

    Double offset amount for scroll up/down commands.

--- src/commands/view.rs
+++ src/commands/view.rs
@@ -4,14 +4,14 @@ use models::application::Application;
 
 pub fn scroll_up(app: &mut Application) {
     match app.workspace.current_buffer() {
-        Some(ref buffer) => app.buffer_view.scroll_up(buffer, 5),
+        Some(ref buffer) => app.buffer_view.scroll_up(buffer, 10),
         None => ()
     }
 }
 
 pub fn scroll_down(app: &mut Application) {
     match app.workspace.current_buffer() {
-        Some(ref buffer) => app.buffer_view.scroll_down(buffer, 5),
+        Some(ref buffer) => app.buffer_view.scroll_down(buffer, 10),
         None => ()
     }
 }
