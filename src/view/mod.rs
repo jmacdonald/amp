41b5c42512dcc6d2613ca86879b6027a0a71b7dc
Jordan MacDonald <jordan@wastedintelligence.com>
2015-11-04 23:33:27 -0500

    Add colors to other token types.

--- src/view/mod.rs
+++ src/view/mod.rs
@@ -24,15 +24,21 @@ pub struct StatusLine {
 
 pub fn map_color(category: &Category) -> Color {
     match category {
-        &Category::Keyword    => Color::Yellow,
-        &Category::Identifier => Color::Magenta,
-        &Category::String     => Color::Red,
-        &Category::Key        => Color::Red,
-        &Category::Comment    => Color::Blue,
-        &Category::Method     => Color::Cyan,
-        &Category::Function   => Color::Cyan,
-        &Category::Call       => Color::Cyan,
-        _                     => Color::Default,
+        &Category::Keyword     => Color::Yellow,
+        &Category::Identifier  => Color::Magenta,
+        &Category::String      => Color::Red,
+        &Category::Key         => Color::Red,
+        &Category::Literal     => Color::Red,
+        &Category::Boolean     => Color::Red,
+        &Category::Comment     => Color::Blue,
+        &Category::Method      => Color::Cyan,
+        &Category::Function    => Color::Cyan,
+        &Category::Call        => Color::Cyan,
+        &Category::Brace       => Color::Cyan,
+        &Category::Bracket     => Color::Cyan,
+        &Category::Parenthesis => Color::Cyan,
+        &Category::Operator    => Color::Cyan,
+        _                      => Color::Default,
     }
 }
 
