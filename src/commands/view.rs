28790de2b5816e66749d396e73098269844f48f4
Jordan MacDonald <jordan@wastedintelligence.com>
2015-12-20 17:16:32 -0500

    Update view commands to use view scroll methods.

--- src/commands/view.rs
+++ src/commands/view.rs
@@ -5,21 +5,21 @@ use models::application::Application;
 
 pub fn scroll_up(app: &mut Application) {
     match app.workspace.current_buffer() {
-        Some(ref buffer) => app.view.buffer_view.scroll_up(buffer, 10),
+        Some(ref buffer) => app.view.scroll_up(buffer, 10),
         None => ()
     }
 }
 
 pub fn scroll_down(app: &mut Application) {
     match app.workspace.current_buffer() {
-        Some(ref buffer) => app.view.buffer_view.scroll_down(buffer, 10),
+        Some(ref buffer) => app.view.scroll_down(buffer, 10),
         None => ()
     }
 }
 
 pub fn scroll_to_cursor(app: &mut Application) {
     match app.workspace.current_buffer() {
-        Some(ref buffer) => app.view.buffer_view.scroll_to_cursor(buffer),
+        Some(ref buffer) => app.view.scroll_to_cursor(buffer),
         None => (),
      }
 }
