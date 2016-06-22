9e9172abda692e974f0e08a6414d6ee87be8e82d
Jordan MacDonald <jordan@wastedintelligence.com>
2016-06-22 00:27:01 -0400

    Clean up normal mode presenter's relative logic.

--- src/presenters/modes/normal.rs
+++ src/presenters/modes/normal.rs
@@ -2,10 +2,9 @@ extern crate scribe;
 extern crate rustbox;
 extern crate git2;
 
-use scribe::buffer::{Buffer, Position};
-use presenters::{buffer_status_line_data, git_status_line_data, visible_tokens};
-use view::{BufferData, StatusLineData, View};
-use view::scrollable_region::Visibility;
+use scribe::Buffer;
+use presenters::{buffer_status_line_data, git_status_line_data};
+use view::{StatusLineData, View};
 use rustbox::Color;
 use git2::Repository;
 
@@ -14,38 +13,6 @@ pub fn display(buffer: Option<&mut Buffer>, view: &mut View, repo: &Option<Repos
     view.clear();
 
     if let Some(buf) = buffer {
-        let line_offset = view.visible_region(buf).line_offset();
-        let visible_range = view.visible_region(buf).visible_range();
-
-        // Get the buffer's tokens and reduce them to the visible set.
-        let visible_tokens = visible_tokens(&buf.tokens(), visible_range);
-
-        // The buffer tracks its cursor absolutely, but the view must display it
-        // relative to any scrolling. Given that, it may also be outside the
-        // visible range, at which point we'll use a None value.
-        let relative_cursor = match view.visible_region(buf)
-                                        .relative_position(buf.cursor.line) {
-            Visibility::Visible(line) => {
-                Some(Position {
-                    line: line,
-                    offset: buf.cursor.offset,
-                })
-            }
-            _ => None,
-        };
-
-        // Bundle up the presentable data.
-        let data = BufferData {
-            tokens: Some(buf.tokens()),
-            cursor: Some(*buf.cursor),
-            highlight: None,
-            line_count: buf.line_count(),
-            scrolling_offset: line_offset,
-        };
-
-        // Handle cursor updates.
-        view.set_cursor(data.cursor);
-
         // Draw the visible set of tokens to the terminal.
         view.draw_absolute_buffer(buf, None);
 
