a88749464681a8b35fc9eb06f054ce19659810c3
Jordan MacDonald <jordan@wastedintelligence.com>
2016-09-17 11:08:06 -0400

    Implement test terminal data method / fix bugs
    The data method is what we'll use to verify printing behaviour.
    Implementing this exposed a handful of bugs in the original printing
    logic, which have been addressed. There are now tests on this test type;
    oh, dear.

--- src/view/terminal/test_terminal.rs
+++ src/view/terminal/test_terminal.rs
@@ -7,25 +7,61 @@ use rustbox::{Color, Event, InitOptions, RustBox, Style};
 const WIDTH: usize = 10;
 const HEIGHT: usize = 10;
 
+// A headless terminal that tracks printed data, which can be
+// returned as a String to test display logic of other types.
 pub struct TestTerminal {
-    data: RefCell<[[char; HEIGHT]; WIDTH]>, // 2D array of chars to represent screen
+    data: RefCell<[[Option<char>; WIDTH]; HEIGHT]>, // 2D array of chars to represent screen
     cursor: Cell<Option<Position>>,
 }
 
 impl TestTerminal {
     pub fn new() -> TestTerminal {
         TestTerminal {
-            data: RefCell::new([[' '; WIDTH]; HEIGHT]),
+            data: RefCell::new([[None; WIDTH]; HEIGHT]),
             cursor: Cell::new(None)
         }
+    }
+
+    // Returns a String representation of the printed data.
+    pub fn data(&self) -> String {
+        let mut data = String::new();
+        let mut last_row_with_data = 0;
+        let mut last_column_with_data = 0;
+
+        for (y, row) in self.data.borrow().iter().enumerate() {
+            for (x, cell) in row.iter().enumerate() {
+                if let Some(c) = *cell {
+                    for _ in last_row_with_data..y {
+                        data.push('\n');
+                        last_column_with_data = 0;
+                    }
+
+                    for _ in last_column_with_data..x {
+                        data.push(' ');
+                    }
+
+                    data.push(c);
+
+                    last_row_with_data = y;
+
+                    // Since the column changes on each character, and we don't
+                    // want to print a space in between every character, we
+                    // set it ahead when we've run into a character to
+                    // differentiate from leading spaces.
+                    last_column_with_data = x+1;
+                }
+            }
+        }
+
+        data
     }
 }
 
 impl Terminal for TestTerminal {
     fn listen(&self) -> Event { Event::NoEvent }
     fn clear(&self) {
-        for column in self.data.borrow_mut().iter_mut() {
-            *column = [' '; HEIGHT];
+        for row in self.data.borrow_mut().iter_mut() {
+            *row = [None; WIDTH];
         }
     }
     fn present(&self) { }
@@ -37,13 +73,50 @@ impl Terminal for TestTerminal {
     fn print(&self, x: usize, y: usize, style: Style, fg: Color, bg: Color, s: &str) {
         let mut data = self.data.borrow_mut();
 
-        for c in s.chars() {
-            data[x][y] = c;
+        for (i, c) in s.chars().enumerate() {
+            data[y][i+x] = Some(c);
         }
     }
 
     fn print_char(&self, x: usize, y: usize, style: Style, fg: Color, bg: Color, c: char) {
-        self.data.borrow_mut()[x][y] = c;
+        self.data.borrow_mut()[y][x] = Some(c);
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use view::terminal::Terminal;
+    use super::TestTerminal;
+    use rustbox;
+    use rustbox::Color;
+
+    #[test]
+    fn print_sets_terminal_data_correctly() {
+        let terminal = TestTerminal::new();
+        terminal.print(0, 0, rustbox::RB_NORMAL, Color::Black, Color::White, "data");
+
+        assert_eq!(terminal.data(), "data");
+    }
+
+    #[test]
+    fn print_char_sets_terminal_data_correctly() {
+        let terminal = TestTerminal::new();
+        terminal.print_char(0, 0, rustbox::RB_NORMAL, Color::Black, Color::White, 'd');
+        terminal.print_char(1, 0, rustbox::RB_NORMAL, Color::Black, Color::White, 'a');
+        terminal.print_char(2, 0, rustbox::RB_NORMAL, Color::Black, Color::White, 't');
+        terminal.print_char(3, 0, rustbox::RB_NORMAL, Color::Black, Color::White, 'a');
+
+        assert_eq!(terminal.data(), "data");
+    }
+
+    #[test]
+    fn data_uses_newlines_and_spaces_to_represent_structure() {
+        let terminal = TestTerminal::new();
+
+        // Setting a non-zero x coordinate on a previous line exercises column resetting.
+        terminal.print(2, 0, rustbox::RB_NORMAL, Color::Black, Color::White, "some");
+        terminal.print(5, 2, rustbox::RB_NORMAL, Color::Black, Color::White, "data");
+
+        assert_eq!(terminal.data(), "  some\n\n     data");
+    }
+}
