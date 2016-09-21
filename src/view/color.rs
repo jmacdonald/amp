cfe84fddbe96bf0cdca061369cc731501bf10677
Jordan MacDonald <jordan@wastedintelligence.com>
2016-09-20 21:28:33 -0400

    Add placeholder color for keywords

--- src/view/color.rs
+++ src/view/color.rs
@@ -4,6 +4,8 @@ use scribe::buffer::Scope;
 pub fn map(scope: &Scope) -> Color {
     if scope.is_prefix_of(Scope::new("string.quoted").unwrap()) {
         Color::Red
+    } else if scope.is_prefix_of(Scope::new("keyword").unwrap()) {
+        Color::Yellow
     } else {
         Color::Default
     }
