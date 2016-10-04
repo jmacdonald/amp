adcbd87246837d7cd833f8ca6a81048ac9757e64
Jordan MacDonald <jordan@wastedintelligence.com>
2016-10-03 21:14:20 -0400

    Implement copy/clone on style/color types

--- src/view/color.rs
+++ src/view/color.rs
@@ -1,8 +1,10 @@
+use std::clone::Clone;
 pub use termion::color::Rgb as RGBColor;
 
 /// A convenience type used to represent a foreground/background
 /// color combination. Provides generic/convenience variants to
 /// discourage color selection outside of the theme, whenever possible.
+#[derive(Clone)]
 pub enum Colors {
     Blank,    // blank/blank
     Default,  // default/background
--- src/view/style.rs
+++ src/view/style.rs
@@ -1,3 +1,4 @@
+#[derive(Copy, Clone)]
 pub enum Style {
     Default,
     Bold,
