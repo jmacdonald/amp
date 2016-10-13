bbb5fa9424d37170e4a4a91c4bf434fd527fea91
Jordan MacDonald <jordan@wastedintelligence.com>
2016-10-12 21:04:45 -0400

    Persist style changes!

--- src/view/buffer_renderer.rs
+++ src/view/buffer_renderer.rs
@@ -152,7 +152,7 @@ impl<'a, 'b> BufferRenderer<'a, 'b> {
     // Uses the lexeme's scopes to update the current
     // style, so that print calls will use the right color.
     fn update_current_style(&mut self, lexeme: &Lexeme) {
-        self.current_style.apply(
+        self.current_style = self.current_style.apply(
             self.stylist.get_style(
                 lexeme.scope.as_slice()
             )
