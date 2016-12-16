b5243d0274810c56b0e9870c75350b42310f4576
Jordan MacDonald <jordan@wastedintelligence.com>
2016-12-15 20:44:18 -0500

    Implement result handling for symbol jump mode commands

--- src/commands/symbol_jump.rs
+++ src/commands/symbol_jump.rs
@@ -1,46 +1,58 @@
 extern crate bloodhound;
 extern crate scribe;
 
+use errors::*;
 use commands;
 use commands::Result;
 use models::application::{Application, Mode};
 
 pub fn jump_to_selected_symbol(app: &mut Application) -> Result {
     if let Mode::SymbolJump(ref mut mode) = app.mode {
-        if let Some(buf) = app.workspace.current_buffer() {
-            if let Some(position) = mode.selected_symbol_position() {
-                buf.cursor.move_to(position);
-            }
+        let buffer = app
+            .workspace
+            .current_buffer()
+            .ok_or(BUFFER_MISSING)?;
+
+        let position = mode
+            .selected_symbol_position()
+            .ok_or("Couldn't find a position for the selected symbol")?;
+
+        if !buffer.cursor.move_to(position) {
+            bail!("Couldn't move to the selected symbol's position");
         }
     }
-    commands::view::scroll_cursor_to_center(app);
-    commands::application::switch_to_normal_mode(app);
+
+    commands::view::scroll_cursor_to_center(app)?;
+    commands::application::switch_to_normal_mode(app)?;
 
     Ok(())
 }
 
 pub fn search(app: &mut Application) -> Result {
-    match app.mode {
-        Mode::SymbolJump(ref mut mode) => mode.search(),
-        _ => (),
+    if let Mode::SymbolJump(ref mut mode) = app.mode {
+        mode.search();
+    } else {
+        bail!("Can't search symbols outside of symbol jump mode");
     }
 
     Ok(())
 }
 
 pub fn select_next_symbol(app: &mut Application) -> Result {
-    match app.mode {
-        Mode::SymbolJump(ref mut mode) => mode.results.select_next(),
-        _ => (),
+    if let Mode::SymbolJump(ref mut mode) = app.mode {
+        mode.results.select_next();
+    } else {
+        bail!("Can't change symbol selection outside of symbol jump mode");
     }
 
     Ok(())
 }
 
 pub fn select_previous_symbol(app: &mut Application) -> Result {
-    match app.mode {
-        Mode::SymbolJump(ref mut mode) => mode.results.select_previous(),
-        _ => (),
+    if let Mode::SymbolJump(ref mut mode) = app.mode {
+        mode.results.select_previous();
+    } else {
+        bail!("Can't change symbol selection outside of symbol jump mode");
     }
 
     Ok(())
@@ -49,6 +61,8 @@ pub fn select_previous_symbol(app: &mut Application) -> Result {
 pub fn enable_insert(app: &mut Application) -> Result {
     if let Mode::SymbolJump(ref mut mode) = app.mode {
         mode.insert = true;
+    } else {
+        bail!("Can't change symbol search insert state outside of symbol jump mode");
     }
 
     Ok(())
@@ -57,6 +71,8 @@ pub fn enable_insert(app: &mut Application) -> Result {
 pub fn disable_insert(app: &mut Application) -> Result {
     if let Mode::SymbolJump(ref mut mode) = app.mode {
         mode.insert = false;
+    } else {
+        bail!("Can't change symbol search insert state outside of symbol jump mode");
     }
 
     Ok(())
