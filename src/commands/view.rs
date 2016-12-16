464c991a2efd2dfd866b4b8943e2390442c5703a
Jordan MacDonald <jordan@wastedintelligence.com>
2016-12-15 20:51:00 -0500

    Implement result handling for view commands

--- src/commands/view.rs
+++ src/commands/view.rs
@@ -1,40 +1,45 @@
 extern crate scribe;
 
+use errors::*;
 use commands::Result;
 use models::application::Application;
 
 pub fn scroll_up(app: &mut Application) -> Result {
-    match app.workspace.current_buffer() {
-        Some(ref buffer) => app.view.scroll_up(buffer, 10),
-        None => (),
-    }
+    let buffer = app
+        .workspace
+        .current_buffer()
+        .ok_or(BUFFER_MISSING)?;
 
+    app.view.scroll_up(buffer, 10);
     Ok(())
 }
 
 pub fn scroll_down(app: &mut Application) -> Result {
-    match app.workspace.current_buffer() {
-        Some(ref buffer) => app.view.scroll_down(buffer, 10),
-        None => (),
-    }
+    let buffer = app
+        .workspace
+        .current_buffer()
+        .ok_or(BUFFER_MISSING)?;
 
+    app.view.scroll_down(buffer, 10);
     Ok(())
 }
 
 pub fn scroll_to_cursor(app: &mut Application) -> Result {
-    match app.workspace.current_buffer() {
-        Some(ref buffer) => app.view.scroll_to_cursor(buffer),
-        None => (),
-    }
+    let buffer = app
+        .workspace
+        .current_buffer()
+        .ok_or(BUFFER_MISSING)?;
 
+    app.view.scroll_to_cursor(buffer);
     Ok(())
 }
 
 pub fn scroll_cursor_to_center(app: &mut Application) -> Result {
-    match app.workspace.current_buffer() {
-        Some(ref buffer) => app.view.scroll_to_center(buffer),
-        None => (),
-    }
+    let buffer = app
+        .workspace
+        .current_buffer()
+        .ok_or(BUFFER_MISSING)?;
 
+    app.view.scroll_to_center(buffer);
     Ok(())
 }
