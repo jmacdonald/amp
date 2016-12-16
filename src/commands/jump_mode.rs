ac06f96012de7f2af376acce20d8ca5ae48844b2
Jordan MacDonald <jordan@wastedintelligence.com>
2016-12-15 20:18:49 -0500

    Implement result handling for jump mode commands

--- src/commands/jump_mode.rs
+++ src/commands/jump_mode.rs
@@ -1,3 +1,4 @@
+use errors::*;
 use std::mem;
 use commands::Result;
 use models::application::modes::jump;
@@ -11,13 +12,15 @@ pub fn match_tag(app: &mut Application) -> Result {
             0 => return Ok(()), // Not enough data to match to a position.
             1 => {
                 if jump_mode.first_phase {
-                    jump_to_tag(jump_mode, &mut app.workspace);
+                    jump_to_tag(jump_mode, &mut app.workspace)?
                 } else {
                     return Ok(()) // Not enough data to match to a position.
                 }
             },
-            _ => jump_to_tag(jump_mode, &mut app.workspace),
+            _ => jump_to_tag(jump_mode, &mut app.workspace)?,
         }
+    } else {
+        bail!("Can't match jump tags outside of jump mode.");
     };
 
     switch_to_previous_mode(app);
@@ -26,12 +29,19 @@ pub fn match_tag(app: &mut Application) -> Result {
 }
 
 // Try to find a position for the input tag and jump to it.
-fn jump_to_tag(jump_mode: &mut JumpMode, workspace: &mut Workspace) {
-    if let Some(position) = jump_mode.map_tag(&jump_mode.input) {
-        if let Some(buf) = workspace.current_buffer(){
-            buf.cursor.move_to(position.clone());
-        };
+fn jump_to_tag(jump_mode: &mut JumpMode, workspace: &mut Workspace) -> Result {
+    let position = jump_mode
+        .map_tag(&jump_mode.input)
+        .ok_or("Couldn't find a position for the specified tag")?;
+    let buffer = workspace
+        .current_buffer()
+        .ok_or(BUFFER_MISSING)?;
+
+    if !buffer.cursor.move_to(position.clone()) {
+        bail!("Couldn't move to the specified tag's position")
     }
+
+    Ok(())
 }
 
 fn switch_to_previous_mode(app: &mut Application) {
@@ -39,18 +49,15 @@ fn switch_to_previous_mode(app: &mut Application) {
 
     // Now that we own the jump mode, switch to
     // the previous select mode, if there was one.
-    match old_mode {
-        Mode::Jump(jump_mode) => {
-            match jump_mode.select_mode {
-                jump::SelectModeOptions::None => (),
-                jump::SelectModeOptions::Select(select_mode) => {
-                    app.mode = Mode::Select(select_mode);
-                }
-                jump::SelectModeOptions::SelectLine(select_mode) => {
-                    app.mode = Mode::SelectLine(select_mode);
-                }
+    if let Mode::Jump(jump_mode) = old_mode {
+        match jump_mode.select_mode {
+            jump::SelectModeOptions::None => (),
+            jump::SelectModeOptions::Select(select_mode) => {
+                app.mode = Mode::Select(select_mode);
+            }
+            jump::SelectModeOptions::SelectLine(select_mode) => {
+                app.mode = Mode::SelectLine(select_mode);
             }
         }
-        _ => (),
     }
 }
