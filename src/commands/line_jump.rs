67037925be31ad96a289a0de6cdfa31e4022d09c
Jordan MacDonald <jordan@wastedintelligence.com>
2016-12-15 19:55:52 -0500

    Implement result handling for line jump commands

--- src/commands/line_jump.rs
+++ src/commands/line_jump.rs
@@ -1,55 +1,55 @@
-extern crate scribe;
-
+use errors::*;
 use commands;
 use commands::Result;
 use models::application::{Application, Mode};
 use scribe::buffer::Position;
 
 pub fn accept_input(app: &mut Application) -> Result {
-    match app.mode {
-        Mode::LineJump(ref mode) => {
-            // Try parsing an integer from the input.
-            match mode.input.parse::<usize>() {
-                Ok(line_number) => {
-                    // Ignore zero-value line numbers.
-                    if line_number > 0 {
-                        match app.workspace.current_buffer() {
-                            Some(buffer) => {
-                                // Input values won't be zero-indexed; map the value so
-                                // that we can use it for a zero-indexed buffer position.
-                                let target_line = line_number - 1;
+    if let Mode::LineJump(ref mode) = app.mode {
+        // Try parsing an integer from the input.
+        let line_number = mode
+            .input
+            .parse::<usize>()
+            .chain_err(|| "Couldn't parse a line number from the provided input.")?;
 
-                                // Build an ideal target position to which we'll try moving.
-                                let mut target_position = Position {
-                                    line: target_line,
-                                    offset: buffer.cursor.offset,
-                                };
+        // Ignore zero-value line numbers.
+        if line_number > 0 {
+            let buffer = app
+                .workspace
+                .current_buffer()
+                .ok_or(BUFFER_MISSING)?;
 
-                                if !buffer.cursor.move_to(target_position) {
-                                    // Moving to that position failed. It may be because the
-                                    // current offset doesn't exist there. Try falling back
-                                    // to the end of the target line.
-                                    match buffer.data().lines().nth(target_line) {
-                                        Some(line_content) => {
-                                            target_position.offset = line_content.len();
-                                            buffer.cursor.move_to(target_position);
-                                        }
-                                        None => (),
-                                    }
-                                }
-                            }
-                            None => (),
-                        }
-                    }
-                }
-                Err(_) => (),
+            // Input values won't be zero-indexed; map the value so
+            // that we can use it for a zero-indexed buffer position.
+            let target_line = line_number - 1;
+
+            // Build an ideal target position to which we'll try moving.
+            let mut target_position = Position {
+                line: target_line,
+                offset: buffer.cursor.offset,
+            };
+
+            if !buffer.cursor.move_to(target_position) {
+                // Moving to that position failed. It may be because the
+                // current offset doesn't exist there. Try falling back
+                // to the end of the target line.
+                let line_content = buffer
+                    .data()
+                    .lines()
+                    .nth(target_line)
+                    .map(|line| line.to_string())
+                    .ok_or("Couldn't find the specified line")?;
+
+                target_position.offset = line_content.len();
+                buffer.cursor.move_to(target_position);
             }
         }
-        _ => (),
+    } else {
+        bail!("Can't accept line jump input outside of line jump mode.");
     }
 
-    commands::application::switch_to_normal_mode(app);
-    commands::view::scroll_cursor_to_center(app);
+    commands::application::switch_to_normal_mode(app)?;
+    commands::view::scroll_cursor_to_center(app)?;
 
     Ok(())
 }
--- src/errors.rs
+++ src/errors.rs
@@ -1,2 +1,4 @@
 // Create the Error, ErrorKind, ResultExt, and Result types
 error_chain! { }
+
+pub static BUFFER_MISSING: &'static str = "No buffer available";
