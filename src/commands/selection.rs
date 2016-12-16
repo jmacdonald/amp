0aeada810cbbc5af28111b9dcfe59fad263b3b41
Jordan MacDonald <jordan@wastedintelligence.com>
2016-12-15 21:23:26 -0500

    Implement result handling for selection commands

--- src/commands/selection.rs
+++ src/commands/selection.rs
@@ -3,33 +3,33 @@ extern crate scribe;
 use models::application::{Application, ClipboardContent, Mode};
 use scribe::buffer::{LineRange, Range};
 use super::application;
+use errors::*;
 use commands;
 use commands::Result;
 use helpers;
 
 pub fn delete(app: &mut Application) -> Result {
-    match app.workspace.current_buffer() {
-        Some(buffer) => {
-            match app.mode {
-                Mode::Select(ref select_mode) => {
-                    let cursor_position = *buffer.cursor.clone();
-                    let delete_range = Range::new(cursor_position, select_mode.anchor);
-                    buffer.delete_range(delete_range.clone());
-                    buffer.cursor.move_to(delete_range.start());
-                }
-                Mode::SelectLine(ref mode) => {
-                    let delete_range = mode.to_range(&*buffer.cursor);
-                    buffer.delete_range(delete_range.clone());
-                    buffer.cursor.move_to(delete_range.start());
-                }
-                _ => (),
-            };
-        }
-        None => (),
-    };
+    if let Some(buffer) = app.workspace.current_buffer() {
+        match app.mode {
+            Mode::Select(ref select_mode) => {
+                let cursor_position = *buffer.cursor.clone();
+                let delete_range = Range::new(cursor_position, select_mode.anchor);
+                buffer.delete_range(delete_range.clone());
+                buffer.cursor.move_to(delete_range.start());
+            }
+            Mode::SelectLine(ref mode) => {
+                let delete_range = mode.to_range(&*buffer.cursor);
+                buffer.delete_range(delete_range.clone());
+                buffer.cursor.move_to(delete_range.start());
+            }
+            _ => bail!("Can't delete selections outside of select mode"),
+        };
+    } else {
+        bail!(BUFFER_MISSING);
+    }
 
-    application::switch_to_normal_mode(app);
-    commands::view::scroll_to_cursor(app);
+    application::switch_to_normal_mode(app)?;
+    commands::view::scroll_to_cursor(app)?;
 
     Ok(())
 }
@@ -58,37 +58,35 @@ pub fn copy(app: &mut Application) -> Result {
 }
 
 fn copy_to_clipboard(app: &mut Application) -> Result {
-    match app.workspace.current_buffer() {
-        Some(buffer) => {
-            match app.mode {
-                Mode::Select(ref select_mode) => {
-                    let cursor_position = *buffer.cursor.clone();
-                    let selected_range = Range::new(cursor_position, select_mode.anchor);
+    let buffer = app
+        .workspace
+        .current_buffer()
+        .ok_or(BUFFER_MISSING)?;
 
-                    match buffer.read(&selected_range.clone()) {
-                        Some(selected_data) => {
-                            app.clipboard.set_content(ClipboardContent::Inline(selected_data))
-                        }
-                        None => (),
-                    }
-                }
-                Mode::SelectLine(ref mode) => {
-                    let selected_range = helpers::inclusive_range(&LineRange::new(mode.anchor,
-                                                                                  buffer.cursor
-                                                                                        .line),
-                                                                  buffer);
+    match app.mode {
+        Mode::Select(ref select_mode) => {
+            let cursor_position = *buffer.cursor.clone();
+            let selected_range = Range::new(cursor_position, select_mode.anchor);
 
-                    match buffer.read(&selected_range.clone()) {
-                        Some(selected_data) => {
-                            app.clipboard.set_content(ClipboardContent::Block(selected_data))
-                        }
-                        None => (),
-                    }
-                }
-                _ => (),
-            };
+            let data = buffer.read(&selected_range.clone())
+                .ok_or("Couldn't read selected data from buffer")?;
+            app.clipboard.set_content(ClipboardContent::Inline(data));
         }
-        None => (),
+        Mode::SelectLine(ref mode) => {
+            let selected_range = helpers::inclusive_range(
+                &LineRange::new(
+                    mode.anchor,
+                    buffer.cursor
+                    .line
+                ),
+                buffer
+            );
+
+            let data = buffer.read(&selected_range.clone())
+                .ok_or("Couldn't read selected data from buffer")?;
+            app.clipboard.set_content(ClipboardContent::Block(data));
+        }
+        _ => bail!("Can't copy data to clipboard outside of select modes"),
     };
 
     Ok(())
