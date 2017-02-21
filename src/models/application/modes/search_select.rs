cad0aff9644b25370d35ca170a32f03914f562fd
Jordan MacDonald <jordan@wastedintelligence.com>
2017-02-20 23:03:24 -0500

    Initial SearchSelectMode trait implementation
    This is the interface to which open, symbol jump, and theme select modes
    will eventually conform. It'll allow us to DRY up the commands,
    presenters, and input handlers associated with these modes.

---
+++ src/models/application/modes/search_select.rs
@@ -0,0 +1,85 @@
+use std::fmt::Display;
+
+/// This trait will become vastly simpler if/when fields are added to traits.
+/// See: https://github.com/rust-lang/rfcs/pull/1546
+pub trait SearchSelectMode<T: Display> {
+    fn query(&mut self) -> &mut String;
+    fn search(&mut self);
+    fn insert_mode(&self) -> bool;
+    fn set_insert_mode(&mut self, insert_mode: bool);
+    fn selection(&self) -> &T;
+    fn select_previous(&mut self);
+    fn select_next(&mut self);
+    fn push_search_char(&mut self, c: char) {
+        self.query().push(c);
+    }
+    fn pop_search_token(&mut self) {
+        let mut query = self.query();
+
+        // Find the last word boundary (transition to/from whitespace), using
+        // using fold to carry the previous character's type forward.
+        let mut boundary_index = 0;
+        query.char_indices().fold(true, |was_whitespace, (index, c)| {
+            if c.is_whitespace() && !was_whitespace {
+                boundary_index = index;
+            } else if !c.is_whitespace() && was_whitespace {
+                boundary_index = index;
+            }
+
+            c.is_whitespace()
+        });
+
+        query.truncate(boundary_index);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SearchSelectMode;
+
+    struct TestMode {
+        input: String,
+        selection: String
+    }
+
+    impl SearchSelectMode<String> for TestMode {
+        fn query(&mut self) -> &mut String {
+            &mut self.input
+        }
+
+        fn search(&mut self) { }
+        fn insert_mode(&self) -> bool { false }
+        fn set_insert_mode(&mut self, insert_mode: bool) { }
+        fn selection(&self) -> &String { &self.selection }
+        fn select_previous(&mut self) { }
+        fn select_next(&mut self) { }
+    }
+
+    #[test]
+    fn push_search_char_updates_query() {
+        let mut mode = TestMode{ input: String::new(), selection: String::new() };
+        mode.push_search_char('a');
+        assert_eq!(mode.query(), "a");
+    }
+
+    #[test]
+    fn pop_search_token_pops_all_characters_when_on_only_token() {
+        let mut mode = TestMode{ input: String::from("amp"), selection: String::new() };
+        mode.pop_search_token();
+        assert_eq!(mode.query(), "");
+    }
+
+    #[test]
+    fn pop_search_token_pops_all_adjacent_non_whitespace_characters_when_on_non_whitespace_character() {
+        let mut mode = TestMode{ input: String::from("amp editor"), selection: String::new() };
+        mode.pop_search_token();
+        assert_eq!(mode.query(), "amp ");
+    }
+
+    #[test]
+    fn pop_search_token_pops_all_whitespace_characters_when_on_whitespace_character() {
+        let mut mode = TestMode{ input: String::from("amp  "), selection: String::new() };
+        mode.pop_search_token();
+        assert_eq!(mode.query(), "amp");
+    }
+}
