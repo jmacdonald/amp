152a180b3e25a39295850f58229d998142a601d6
Jordan MacDonald <jordan@wastedintelligence.com>
2017-03-10 17:31:33 -0500

    Remove unused input handlers and presenters

--- src/presenters/modes/mod.rs
+++ src/presenters/modes/mod.rs
@@ -1,11 +1,9 @@
 pub mod insert;
 pub mod jump;
 pub mod line_jump;
-pub mod symbol_jump;
 pub mod normal;
 pub mod open;
 pub mod search_insert;
 pub mod search_select;
 pub mod select;
 pub mod select_line;
-pub mod theme;
