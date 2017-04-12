30b0e765004ec7209564f2157855e432de98f797
Jordan MacDonald <jordan@wastedintelligence.com>
2017-04-11 21:06:14 -0400

    Exit jump mode before bubbling up errors

--- src/commands/jump_mode.rs
+++ src/commands/jump_mode.rs
@@ -7,25 +7,25 @@ use models::application::{Mode, Application};
 use scribe::Workspace;
 
 pub fn match_tag(app: &mut Application) -> Result {
-    if let Mode::Jump(ref mut jump_mode) = app.mode {
-        match jump_mode.input.len() {
-            0 => return Ok(()), // Not enough data to match to a position.
-            1 => {
-                if jump_mode.first_phase {
-                    jump_to_tag(jump_mode, &mut app.workspace)?
-                } else {
-                    return Ok(()) // Not enough data to match to a position.
-                }
-            },
-            _ => jump_to_tag(jump_mode, &mut app.workspace)?,
-        }
-    } else {
-        bail!("Can't match jump tags outside of jump mode.");
-    };
-
+    let result =
+        if let Mode::Jump(ref mut jump_mode) = app.mode {
+            match jump_mode.input.len() {
+                0 => return Ok(()), // Not enough data to match to a position.
+                1 => {
+                    if jump_mode.first_phase {
+                        jump_to_tag(jump_mode, &mut app.workspace)
+                    } else {
+                        return Ok(()) // Not enough data to match to a position.
+                    }
+                },
+                _ => jump_to_tag(jump_mode, &mut app.workspace),
+            }
+        } else {
+            bail!("Can't match jump tags outside of jump mode.");
+        };
     switch_to_previous_mode(app);
 
-    Ok(())
+    result
 }
 
 // Try to find a position for the input tag and jump to it.
