d1543dee5491266c1a1b866dd41c2e9a66461434
Jordan MacDonald <jordan@wastedintelligence.com>
2017-11-08 00:15:59 -0500

    Initial search command migration to mode-based results

--- src/commands/search.rs
+++ src/commands/search.rs
@@ -4,19 +4,14 @@ use commands::{self, Result};
 use models::application::{Application, Mode};
 
 pub fn move_to_previous_result(app: &mut Application) -> Result {
-    {
-        let query = app
-            .search_query
-            .as_ref()
-            .ok_or("Can't navigate results without a search query")?;
+    if let Mode::Search(ref mode) = app.mode {
         let buffer = app.workspace.current_buffer().ok_or(BUFFER_MISSING)?;
         let initial_position = *buffer.cursor;
-        let positions = buffer.search(query);
 
         // Try to find and move to a result before the cursor.
-        for position in positions.iter().rev() {
-            if position < &*buffer.cursor {
-                buffer.cursor.move_to(*position);
+        for range in mode.results.iter().rev() {
+            if range.start() < *buffer.cursor {
+                buffer.cursor.move_to(range.start());
 
                 // We've found one; stop looking.
                 break;
@@ -26,12 +21,14 @@ pub fn move_to_previous_result(app: &mut Application) -> Result {
         if buffer.cursor.position == initial_position {
             // There's nothing before the cursor, so wrap
             // to the last match, if there are any at all.
-            if let Some(last_position) = positions.last() {
-                buffer.cursor.move_to(*last_position);
+            if let Some(last_range) = mode.results.last() {
+                buffer.cursor.move_to(last_range.start());
             } else {
                 bail!(NO_SEARCH_RESULTS);
             }
         }
+    } else {
+        bail!("Can't move to search result outside of search mode");
     }
 
     commands::view::scroll_cursor_to_center(app)
@@ -41,19 +38,14 @@ pub fn move_to_previous_result(app: &mut Application) -> Result {
 }
 
 pub fn move_to_next_result(app: &mut Application) -> Result {
-    {
-        let query = app
-            .search_query
-            .as_ref()
-            .ok_or("Can't navigate results without a search query")?;
+    if let Mode::Search(ref mode) = app.mode {
         let buffer = app.workspace.current_buffer().ok_or(BUFFER_MISSING)?;
         let initial_position = *buffer.cursor;
-        let positions = buffer.search(query);
 
         // Try to find and move to a result after the cursor.
-        for position in &positions {
-            if position > &buffer.cursor {
-                buffer.cursor.move_to(*position);
+        for range in &mode.results {
+            if range.start() > *buffer.cursor {
+                buffer.cursor.move_to(range.start());
 
                 // We've found one; stop looking.
                 break;
@@ -63,12 +55,14 @@ pub fn move_to_next_result(app: &mut Application) -> Result {
         if buffer.cursor.position == initial_position {
             // We haven't found anything after the cursor, so wrap
             // to the first match, if there are any matches at all.
-            if let Some(position) = positions.first() {
-                buffer.cursor.move_to(*position);
+            if let Some(first_range) = mode.results.first() {
+                buffer.cursor.move_to(first_range.start());
             } else {
                 bail!(NO_SEARCH_RESULTS);
             }
         }
+    } else {
+        bail!("Can't move to search result outside of search mode");
     }
 
     commands::view::scroll_cursor_to_center(app)
@@ -79,11 +73,19 @@ pub fn move_to_next_result(app: &mut Application) -> Result {
 
 pub fn accept_query(app: &mut Application) -> Result {
     let query = match app.mode {
-        Mode::Search(ref mode) => Some(mode.input.clone()),
+        Mode::Search(ref mut mode) => {
+            // Search the buffer.
+            let buffer = app.workspace.current_buffer().ok_or(BUFFER_MISSING)?;
+            mode.search(&buffer);
+
+            // Disable insert sub-mode.
+            mode.insert = false;
+
+            Some(mode.input.clone())
+        },
         _ => None,
     }.ok_or("Can't accept search query outside of search mode")?;
 
-    commands::application::switch_to_normal_mode(app)?;
     app.search_query = Some(query);
     move_to_next_result(app)?;
 
@@ -132,8 +134,12 @@ mod tests {
         buffer.insert("amp editor\nedit\nedit");
         app.workspace.add_buffer(buffer);
 
-        // Set the search query for the application.
-        app.search_query = Some("ed".to_string());
+        // Enter search mode and accept a query.
+        commands::application::switch_to_search_mode(&mut app).unwrap();
+        if let Mode::Search(ref mut mode) = app.mode {
+            mode.input = String::from("ed");
+        }
+        commands::search::accept_query(&mut app).unwrap();
 
         // Move beyond the second result.
         app.workspace.current_buffer().unwrap().cursor.move_to(Position {
@@ -160,8 +166,12 @@ mod tests {
         buffer.insert("amp editor\nedit\nedit");
         app.workspace.add_buffer(buffer);
 
-        // Set the search query for the application.
-        app.search_query = Some("ed".to_string());
+        // Enter search mode and accept a query.
+        commands::application::switch_to_search_mode(&mut app).unwrap();
+        if let Mode::Search(ref mut mode) = app.mode {
+            mode.input = String::from("ed");
+        }
+        commands::search::accept_query(&mut app).unwrap();
 
         // Reverse to the previous result, forcing the wrap.
         commands::search::move_to_previous_result(&mut app).unwrap();
@@ -182,8 +192,12 @@ mod tests {
         buffer.insert("amp editor\nedit\nedit");
         app.workspace.add_buffer(buffer);
 
-        // Set the search query for the application.
-        app.search_query = Some("ed".to_string());
+        // Enter search mode and accept a query.
+        commands::application::switch_to_search_mode(&mut app).unwrap();
+        if let Mode::Search(ref mut mode) = app.mode {
+            mode.input = String::from("ed");
+        }
+        commands::search::accept_query(&mut app).unwrap();
 
         // Advance to the second result.
         commands::search::move_to_next_result(&mut app).unwrap();
@@ -191,8 +205,8 @@ mod tests {
         // Ensure the buffer cursor is at the expected position.
         assert_eq!(*app.workspace.current_buffer().unwrap().cursor,
                    Position {
-                       line: 0,
-                       offset: 4,
+                       line: 1,
+                       offset: 0,
                    });
     }
 
@@ -204,8 +218,12 @@ mod tests {
         buffer.insert("amp editor\nedit\nedit");
         app.workspace.add_buffer(buffer);
 
-        // Set the search query for the application.
-        app.search_query = Some("ed".to_string());
+        // Enter search mode and accept a query.
+        commands::application::switch_to_search_mode(&mut app).unwrap();
+        if let Mode::Search(ref mut mode) = app.mode {
+            mode.input = String::from("ed");
+        }
+        commands::search::accept_query(&mut app).unwrap();
 
         // Move to the end of the document.
         app.workspace.current_buffer().unwrap().cursor.move_to(Position {
@@ -225,8 +243,7 @@ mod tests {
     }
 
     #[test]
-    fn accept_query_sets_application_search_query_switches_to_normal_mode_and_moves_to_first_match
-        () {
+    fn accept_query_sets_application_search_query_disables_insert_sub_mode_and_moves_to_first_match() {
         let mut app = ::models::Application::new().unwrap();
         let mut buffer = Buffer::new();
         buffer.insert("amp editor\nedit\nedit");
@@ -240,9 +257,9 @@ mod tests {
         };
         commands::search::accept_query(&mut app).unwrap();
 
-        // Ensure that we're in normal mode.
+        // Ensure that we've disabled insert sub-mode.
         assert!(match app.mode {
-            ::models::application::Mode::Normal => true,
+            ::models::application::Mode::Search(ref mode) => !mode.insert_mode(),
             _ => false,
         });
 
