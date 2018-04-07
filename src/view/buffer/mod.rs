3c99ac741a964b6a960aa214c38ad666928d9691
Jordan MacDonald <jordan@wastedintelligence.com>
2018-04-06 21:57:45 -0400

    Move ScrollableRegion type into buffer submodule

--- src/view/buffer/mod.rs
+++ src/view/buffer/mod.rs
@@ -1,5 +1,7 @@
 mod buffer_renderer;
 mod lexeme_mapper;
+mod scrollable_region;
 
 pub use self::buffer_renderer::BufferRenderer;
 pub use self::lexeme_mapper::LexemeMapper;
+pub use self::scrollable_region::ScrollableRegion;
