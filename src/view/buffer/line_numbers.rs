18a09db322bbed8fc0b002cdb7fc8fc06b49575e
Jordan MacDonald <jordan@wastedintelligence.com>
2018-04-08 10:16:46 -0400

    Add basic LineNumbers iterator type

--- src/view/buffer/line_numbers.rs
+++ src/view/buffer/line_numbers.rs
@@ -1,7 +1,27 @@
 use scribe::Buffer;
+use std::iter::Iterator;
 
 pub const LINE_NUMBER_GUTTER_MARGIN: usize = 1;
 pub const LINE_NUMBER_GUTTER_PADDING: usize = 2;
+
+pub struct LineNumbers {
+    current_number: usize
+}
+
+impl LineNumbers {
+    pub fn new() -> LineNumbers {
+        LineNumbers{ current_number: 0 }
+    }
+}
+
+impl Iterator for LineNumbers {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<usize> {
+        self.current_number += 1;
+        Some(self.current_number)
+    }
+}
 
 pub fn line_number_width(buffer: &Buffer) -> usize {
     buffer.line_count().to_string().len()
@@ -20,5 +40,18 @@ mod tests {
         }
 
         assert_eq!(line_number_width(&buffer), 3);
+    }
+
+    #[test]
+    fn line_numbers_start_at_one() {
+        let mut line_numbers = LineNumbers::new();
+        assert_eq!(line_numbers.next(), Some(1));
+    }
+
+    #[test]
+    fn line_numbers_increment_by_one() {
+        let mut line_numbers = LineNumbers::new();
+        line_numbers.next();
+        assert_eq!(line_numbers.next(), Some(2));
     }
 }
