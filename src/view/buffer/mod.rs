6e9ab93d8bb8f1372f06a63af6f11c3ad38451e1
Jordan MacDonald jordan@wastedintelligence.com <jordan@wastedintelligence.com>
2018-07-09 14:42:27 -0400

    Drop buffer_ prefix from renderer module
    The parent module implies the buffer relationship, so this prefix is
    superfluous.

--- src/view/buffer/mod.rs
+++ src/view/buffer/mod.rs
@@ -1,9 +1,9 @@
-mod buffer_renderer;
+mod renderer;
 mod lexeme_mapper;
 mod line_numbers;
 mod scrollable_region;
 
-pub use self::buffer_renderer::BufferRenderer;
+pub use self::renderer::BufferRenderer;
 pub use self::lexeme_mapper::{LexemeMapper, MappedLexeme};
 pub use self::line_numbers::LineNumbers;
 pub use self::scrollable_region::ScrollableRegion;
