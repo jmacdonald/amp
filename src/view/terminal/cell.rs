7a272f3d07c2eddc085da44fbdb0236413515f78
Jordan MacDonald <jordan@wastedintelligence.com>
2019-01-13 16:32:12 -0500

    Update Cell to use single-char blank string for default content

--- src/view/terminal/cell.rs
+++ src/view/terminal/cell.rs
@@ -1,8 +1,18 @@
 use crate::view::{Colors, Style};
 
-#[derive(Clone, Default, PartialEq)]
+#[derive(Clone, PartialEq)]
 pub struct Cell<'c> {
     pub content: &'c str,
     pub colors: Colors,
     pub style: Style,
+}
+
+impl<'c> Default for Cell<'c> {
+    fn default() -> Self {
+        Cell{
+            content: " ",
+            colors: Colors::default(),
+            style: Style::default()
+        }
+    }
 }
