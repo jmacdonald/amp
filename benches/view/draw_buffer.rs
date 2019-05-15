955275af98dea65aa469ab4ad72211e2eea7af24
Jordan MacDonald <jordan@wastedintelligence.com>
2019-05-14 21:39:36 -0400

    Update benchmarks to work with new presenter-based buffer rendering

--- benches/view/draw_buffer.rs
+++ benches/view/draw_buffer.rs
@@ -12,10 +12,14 @@ fn buffer_rendering(c: &mut Criterion) {
         &PathBuf::from("src/commands/buffer.rs")
     ).unwrap();
     app.view.initialize_buffer(app.workspace.current_buffer().unwrap()).unwrap();
+    let buffer_data = app.workspace.current_buffer().unwrap().data();
 
     c.bench_function("buffer rendering", move |b| b.iter(|| {
-        app.view.draw_buffer(
+        let mut presenter = app.view.build_presenter().unwrap();
+
+        presenter.print_buffer(
             app.workspace.current_buffer().unwrap(),
+            &buffer_data,
             None,
             None
         ).unwrap()
@@ -28,14 +32,18 @@ fn scrolled_buffer_rendering(c: &mut Criterion) {
         &PathBuf::from("src/commands/buffer.rs")
     ).unwrap();
     app.view.initialize_buffer(app.workspace.current_buffer().unwrap()).unwrap();
+    let buffer_data = app.workspace.current_buffer().unwrap().data();
 
     // Scroll to the bottom of the buffer.
     app.workspace.current_buffer().unwrap().cursor.move_to_last_line();
     app.view.scroll_to_cursor(app.workspace.current_buffer().unwrap()).unwrap();
 
     c.bench_function("scrolled buffer rendering", move |b| b.iter(|| {
-        app.view.draw_buffer(
+        let mut presenter = app.view.build_presenter().unwrap();
+
+        presenter.print_buffer(
             app.workspace.current_buffer().unwrap(),
+            &buffer_data,
             None,
             None
         ).unwrap()
