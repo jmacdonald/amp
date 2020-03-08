59e710f7e381262805165769246c758fd7f3f8f7
lberrymage <mageelog@gmail.com>
2020-03-07 18:26:25 -0900

    Remove redundant static lifetimes

--- src/errors.rs
+++ src/errors.rs
@@ -5,9 +5,9 @@ error_chain! {
     }
 }
 
-pub static SEARCH_QUERY_MISSING: &'static str = "No search query";
-pub static NO_SEARCH_RESULTS: &'static str = "No search results available";
-pub static BUFFER_MISSING: &'static str = "No buffer available";
-pub static BUFFER_PATH_MISSING: &'static str = "No path found for the current buffer";
-pub static CURRENT_LINE_MISSING: &'static str = "The current line couldn't be found in the buffer";
-pub static SCROLL_TO_CURSOR_FAILED: &'static str = "Failed to scroll to cursor position";
+pub static SEARCH_QUERY_MISSING: &str = "No search query";
+pub static NO_SEARCH_RESULTS: &str = "No search results available";
+pub static BUFFER_MISSING: &str = "No buffer available";
+pub static BUFFER_PATH_MISSING: &str = "No path found for the current buffer";
+pub static CURRENT_LINE_MISSING: &str = "The current line couldn't be found in the buffer";
+pub static SCROLL_TO_CURSOR_FAILED: &str = "Failed to scroll to cursor position";
