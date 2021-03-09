017a5a00818f900488c8b7e16d0406e5b4046df0
Jordan MacDonald <jordan@wastedintelligence.com>
2021-03-08 22:32:21 -0500

    Qualify usize::MAX reference with std

--- src/commands/buffer.rs
+++ src/commands/buffer.rs
@@ -518,7 +518,7 @@ pub fn toggle_line_comment(app: &mut Application) -> Result {
         // Now fold it into a single (<comment in or out>, <comment offset>) tuple.
         // As soon as <has comment> is `false` a single time, <comment in or out>
         // will result in `false`.
-        .fold((true, usize::MAX), |(folded_toggle, folded_offset), (has_comment, offset)| {
+        .fold((true, std::usize::MAX), |(folded_toggle, folded_offset), (has_comment, offset)| {
             (folded_toggle & has_comment, folded_offset.min(offset))
         });
 
