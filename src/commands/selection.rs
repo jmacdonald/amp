00c701f2edf899af128ab0e99d2734bbf87356fd
Lincoln Auster <lincolnauster@gmail.com>
2021-05-09 12:36:21 -0600

    Update src/commands/selection.rs - use &str instead of &String
    Co-authored-by: Christoph Heiss <me@christoph-heiss.me>

--- src/commands/selection.rs
+++ src/commands/selection.rs
@@ -149,7 +149,7 @@ fn range_from(app: &mut Application) -> Range {
 /// Wrap a string at a given maximum length (generally 80 characters). If the
 /// line begins with a comment (matches potential_prefix), the text is wrapped
 /// around it.
-fn justify_string(text: &String, max_len: usize, potential_prefix: Regex) -> String {
+fn justify_string(text: &str, max_len: usize, potential_prefix: Regex) -> String {
     let mut justified = String::with_capacity(text.len());
     for paragraph in text.split("\n\n") {
         let mut paragraph = paragraph.split_whitespace().peekable();
