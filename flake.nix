2b32008d6423f6d2f4c570749055cd2e05131010
Jordan MacDonald <jordan@wastedintelligence.com>
2024-09-09 22:03:31 -0400

    Migrate Nix build to flakes
    While we're in here, do some cleanup of dependencies and set the build revision
    using Nix's flake revision attribute.

--- flake.nix
+++ flake.nix
@@ -9,10 +9,74 @@
     let
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = nixpkgs.legacyPackages;
     in {
+      # Define packages for all supported systems
       packages = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./. { };
+        default = self.buildPackage { inherit system; };
       });
-    };
+
+      # Define dev shells for all supported systems
+      devShells = forAllSystems (system: {
+        default = self.buildShell { inherit system; };
+      });
+
+      # Function to build a dev shell for a given system
+      buildShell = { system }:
+        let pkgs = import nixpkgs { inherit system; };
+        in pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            cargo-edit
+            rustfmt
+            rust-analyzer
+            clippy
+          ];
+
+          RUST_BACKTRACE = 1;
+        };
+
+      # Function to build the package for a given system
+      buildPackage = { system }:
+        let pkgs = import nixpkgs { inherit system; };
+        in pkgs.rustPlatform.buildRustPackage {
+          pname = "amp";
+          version = "0.7.0";
+          cargoLock.lockFile = ./Cargo.lock;
+
+          # Use source files without version control noise
+          src = pkgs.lib.cleanSource ./.;
+
+          # Packages needed at runtime
+          buildInputs = with pkgs; [ git xorg.libxcb openssl zlib ];
+
+          # Packages needed during the build
+          nativeBuildInputs = with pkgs; [ git ];
+
+          # Make the build/check/install commands explicit so we can
+          # provide the commit SHA for the splash screen
+          buildPhase = ''
+            export BUILD_REVISION=${builtins.substring 0 7 (
+              if self ? rev then self.rev else ""
+            )}
+            echo "BUILD_REVISION=$BUILD_REVISION"
+
+            cargo build --release
+          '';
+
+          checkPhase = ''
+            cargo test
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp target/release/amp $out/bin/
+          '';
+
+          # Amp creates files and directories in $HOME/.config/amp when run.
+          # Since the checkPhase of the build process runs the test suite, we
+          # need a writable location to avoid permission error test failures.
+          HOME="$src";
+        };
+  };
 }
